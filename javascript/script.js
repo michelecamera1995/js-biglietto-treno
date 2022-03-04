18bec3a40ca8e3fc94927c28da582181e1651b3f
michele camera <michy.cam95@gmail.com>
2022-03-04 17:07:19 +0100

    modify

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ const nome = prompt('Inserisci nome');
 
 const km = prompt('km da percorrere');
 
-const età = prompt('Inserisci età');
+const eta = prompt('Inserisci età');
 
 // Calcolo prezzo biglietto
 
@@ -16,21 +16,25 @@ const prezzo18 = Number(42 - 8.4);
 
 const prezzo65 = Number(42 - 16.8);
 
-
-if (età <= 18) {
-    document.getElementById("prezzo").innerHTML = prezzo18;
-}
-
-if (età >= 65) {
-    document.getElementById("prezzo").innerHTML = prezzo65;
-}
-
 // Calcolo sconto prezzo biglietto
 
-document.getElementById("nome").innerHTML = nome + '<span>' + password + '</span>';
+if (eta <= 18) {             // età minore 18
+    document.getElementById("prezzo").innerHTML = prezzo18;
+    console.log("sono entrato nell'if età minori 18");
+} else if (eta >= 65) {   // età maggiore di 65
+    document.getElementById("prezzo").innerHTML = prezzo65;
+    console.log("sono entrato nell'if età maggiore 65");
+} else {                        // età non scontata
+    document.getElementById("prezzo").innerHTML = prezzo;
+    console.log("sono entrato nell'if");
+}
 
-document.getElementById("età").innerHTML = età;
+// Visual
+
+document.getElementById("nome").innerHTML = nome;
+
+document.getElementById("età").innerHTML = eta;
 
 document.getElementById("km").innerHTML = km;
 
-document.getElementById("prezzo").innerHTML = prezzo;
+
